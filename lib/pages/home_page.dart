a9239908e66de4e631ea3b471aa089bdee30adee
Fash Muyhee <fashmuyidavid@gmail.com>
2023-11-28 15:40:05 +0100

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/widgets/product_list.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: ProductList());
+  }
+}
